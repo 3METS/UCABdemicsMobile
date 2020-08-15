4ebaa14180673aebc8086959f601d60bbf2421f6
Miguel Zamora <mike13zp26@hotmail.com>
2020-08-15 06:12:44 -0400

    Pantalla de Seguimiento la que faltaba

--- ucabdemicsmobile/lib/Views/Seguimiento/seguimiento_subject_page.dart
+++ ucabdemicsmobile/lib/Views/Seguimiento/seguimiento_subject_page.dart
@@ -4,14 +4,26 @@ import 'package:ucabdemicsmobile/sources/Constants/constats.dart';
 
 class SeguimientoSubjectPage extends StatefulWidget {
   final String subjectName;
+  final double subjectPercentaje;
 
-  SeguimientoSubjectPage({Key key, @required this.subjectName}) : super(key: key);
+  SeguimientoSubjectPage({Key key, @required this.subjectName, @required this.subjectPercentaje}) : super(key: key);
 
   @override
   _SeguimientoSubjectPageState createState() => _SeguimientoSubjectPageState();
 }
 
 class _SeguimientoSubjectPageState extends State<SeguimientoSubjectPage> {
+
+  double _progressbarvalue;
+
+  Widget percentajeProgress(){
+    setState(() {
+      _progressbarvalue = widget.subjectPercentaje*100;
+    });
+
+    return Text('$_progressbarvalue%', style: TextStyle(color: myBlue, fontWeight: FontWeight.bold), textScaleFactor: MediaQuery.of(context).size.width*0.003,);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +44,96 @@ class _SeguimientoSubjectPageState extends State<SeguimientoSubjectPage> {
         child: ListView(
           children: <Widget>[
             Container(
-              child: CircularProgressIndicator(),
+              padding: EdgeInsets.symmetric(
+                // horizontal: MediaQuery.of(context).size.height*0.17,
+                // vertical: MediaQuery.of(context).size.height*0.042,
+              ),
+              height: MediaQuery.of(context).size.height*0.35,
+              decoration: BoxDecoration(
+                color: myBlue[4],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40.0),
+                  bottomRight: Radius.circular(40.0)),
+              ),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: 40.0,),
+                  Container(
+                    height: MediaQuery.of(context).size.height*0.18,
+                    width: MediaQuery.of(context).size.width*0.3,
+                    child: CircularProgressIndicator(
+                      value: widget.subjectPercentaje,
+                      backgroundColor: Colors.white,
+                      valueColor: new AlwaysStoppedAnimation<Color>(myBlue),
+                      strokeWidth: 20.0,
+                    ),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height*0.03,),
+                  percentajeProgress(),
+                  SizedBox(height: 10,),
+                  Text('Progreso', style: TextStyle(color: myBlue, fontWeight: FontWeight.bold),textScaleFactor: MediaQuery.of(context).size.width*0.003),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    title: Text('Tema 1'),
+                    trailing: IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: null),
+                    subtitle: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: LinearProgressIndicator(
+                            value: 0.25,
+                            backgroundColor: myYellow[1],
+                            valueColor: AlwaysStoppedAnimation<Color>(myYellow[3]),
+                          ),
+                        ),
+                        SizedBox(width: 40,),
+                        Text('25%'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  ListTile(
+                    title: Text('Tema 2'),
+                    trailing: IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: null),
+                    subtitle: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: LinearProgressIndicator(
+                            value: 0.5,
+                            backgroundColor: myYellow[1],
+                            valueColor: AlwaysStoppedAnimation<Color>(myYellow[3]),
+                          ),
+                        ),
+                        SizedBox(width: 40,),
+                        Text('50%'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  ListTile(
+                    title: Text('Tema 3'),
+                    trailing: IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: null),
+                    subtitle: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: LinearProgressIndicator(
+                            value: 0.5,
+                            backgroundColor: myYellow[1],
+                            valueColor: AlwaysStoppedAnimation<Color>(myYellow[3]),
+                          ),
+                        ),
+                        Text('75%'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
